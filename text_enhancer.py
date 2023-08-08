05e5b8efc65e9d41bdcf2d862841baa5b76b8908
Amber Shifflett <amber.z.shifflett@outlook.com>
2023-08-08 02:06:21 -0700

    Debugs the text enhancer.

--- text_enhancer.py
+++ text_enhancer.py
@@ -1,10 +1,13 @@
 print('Welcome to text enhancer. Please enter something. Enter \'q\' to quit.')
 prompt = '//'
+special_char = '+'
 input_string = ''
 input_string = input(prompt)
 while input_string != 'q':
-  for char_index,character in enumerate(input_string[:]):
-    input_string[char_index].insert('+')
-    print(f'Current value: {character}')
-  print('I say: {input_string}')
-  input_string = input(prompt)
+    returnString = ''
+    returnString += special_char
+    for character in input_string:
+        returnString += character
+        returnString += special_char
+    print(f'I say: {returnString}')
+    input_string = input(prompt)
